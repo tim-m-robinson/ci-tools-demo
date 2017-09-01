b2103f8c82c577912aef8b14940d303f9885374d
gogsadmin <RedHat$1>
2017-09-01 14:53:31 +0100

    first commit

---
+++ lib/files/jenkins/docker/default-user.groovy
@@ -0,0 +1,17 @@
+import jenkins.model.*
+import hudson.security.*
+
+def env = System.getenv()
+
+def instance = Jenkins.getInstance()
+
+println "--> creating local user 'admin'"
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount(env.JENKINS_USER, env.JENKINS_PASS)
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy)
+instance.save()
---
+++ lib/files/jenkins/start_jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DOCKER_GID=`getent group docker | awk '{split($0, a, ":"); print a[3]}'`
+echo $DOCKER_GID
+docker rm -f jenkins
+docker run --name jenkins \
+    -v ~/jenkins_home:/var/jenkins_home \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --group-add $DOCKER_GID \
+    -p 9090:8080 \
+-d atos/jenkins:1.0
